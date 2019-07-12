78c91ede3752a31859c079f67603b0f3245dfe7b
Peter van Heusden <pvh@sanbi.ac.za>
2019-07-12 14:33:22 +0200

    add wait_for_dataset, otherer cleanups

--- bulk_upload_to_library.py
+++ bulk_upload_to_library.py
@@ -10,7 +10,8 @@ import shlex
 from pathlib import Path
 from typing import List
 
-from tqdm import tqdmgit
+from tqdm import tqdm
+
 
 class CompressionType(Enum):
     GZIP = 1
@@ -30,12 +31,17 @@ class readable_dir(argparse.Action):
             raise argparse.ArgumentError(self, "readable_dir:{0} is not a readable dir".format(prospective_dir))
 
 
-def create_library(name: str, parsec_cmd: str = 'parsec') -> str:
-    cmd_str = f"{parsec_cmd} libraries create_library '{name}'"
+def safe_run(cmd_str: str, function_name: str) -> subprocess.CompletedProcess:
     cmd = shlex.split(cmd_str)
     proc = subprocess.run(cmd, capture_output=True)
     if proc.returncode:
-        raise OSError(f"create_library failed. cmd_str {cmd_str} return code: {proc.returncode} stderr: {proc.stderr.decode('utf-8')}")
+        raise OSError(f"{function_name} failed: cmd_str {cmd_str} return code: {proc.returncode} stderr: {proc.stderr.decode('utf-8')}\n\nstdout: {proc.stdout.decode('utf-8')}")
+    return proc
+
+
+def create_library(name: str, parsec_cmd: str = 'parsec') -> str:
+    cmd_str = f"{parsec_cmd} libraries create_library '{name}'"
+    proc = safe_run(cmd_str, 'create_library')
     library_details = proc.stdout.decode('utf-8')
     library_id = json.loads(library_details)['id']
     return library_id
@@ -43,10 +49,7 @@ def create_library(name: str, parsec_cmd: str = 'parsec') -> str:
 
 def rename_library_item(item_id: str, new_name: str, parsec_cmd: str = 'parsec') -> None:
     cmd_str = f"{parsec_cmd} libraries update_library_dataset --name '{new_name}' {item_id}"
-    cmd = shlex.split(cmd_str)
-    proc = subprocess.run(cmd, capture_output=True)
-    if proc.returncode:
-        raise OSError(f"rename_library_item failed: cmd_str {cmd_str} return code: {proc.returncode} stderr: {proc.stderr.decode('utf-8')}")
+    safe_run(cmd_str, 'rename_library_item')
     # nothing to return if there is no error
 
 
@@ -57,6 +60,11 @@ def detect_compression(path: Path) -> CompressionType:
         return CompressionType.BZIP2
     else:
         return CompressionType.NONE
+
+
+def wait_for_dataset(library_id: str, dataset_id: str, parsec_cmd: str = 'parsec') -> None:
+    cmd_str = f'parsec libraries wait_for_dataset {library_id} {dataset_id}'
+    safe_run(cmd_str, 'wait_for_dataset')
 
 
 def upload_datasets(datasets_path: Path, library_name: str, dbkey: str, parsec_cmd='parsec') -> None:
@@ -76,13 +84,10 @@ def upload_datasets(datasets_path: Path, library_name: str, dbkey: str, parsec_c
         else:
             raise ValueError(f'Unknown compression type {compression_type}')
         cmd_str = f"{parsec_cmd} libraries upload_file_from_local_path --file_type {format} --dbkey {dbkey} {library_id} '{fastq_file}'"
-        cmd = shlex.split(cmd_str)
         dataset_name = fastq_file.name
         fastq_pos = dataset_name.find('.fastq')
         dataset_name = dataset_name[:fastq_pos]  # strip everything from .fastq onwards
-        proc = subprocess.run(cmd, capture_output=True)
-        if proc.returncode:
-            raise OSError(f'upload failed: cmd_str: {cmd_str} return code: {proc.returncode} stderr: {proc.stderr.decode("utf-8")}')
+        proc = safe_run(cmd_str, 'upload_datasets')
         upload_details = proc.stdout.decode('utf-8')
         # return from a successful upload is a single item list
         # like:
@@ -95,9 +100,8 @@ def upload_datasets(datasets_path: Path, library_name: str, dbkey: str, parsec_c
         #     }
         # ]
         upload_dataset_id = json.loads(upload_details)[0]['id']
-        # TODO: wait for 'ok' state before renaming
-        # use: parsec libraries wait_for_dataset [OPTIONS] LIBRARY_ID DATASET_ID
-        print(f"renaming to {dataset_name}")
+        # wait for Galaxy to process it before renaming it
+        wait_for_dataset(library_id, upload_dataset_id)
         rename_library_item(upload_dataset_id, dataset_name, parsec_cmd)
     # no need to return anything when we are done uploading
 
